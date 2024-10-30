6447f74bb65f9bd08fb30bc8faae3097f0207657
PAlexandre PETRILLO <alexandre-externe.petrillo@gmail.com>
2024-10-30 11:09:05 +0100

    pret pour export pdf facture

--- src/main/java/com/example/demo/service/ExportPdfService.java
+++ src/main/java/com/example/demo/service/ExportPdfService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,10 @@ public class ExportPdfService {
         Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document, outputStream);
         document.open();
+
+        Paragraph paragrapheHeader1 = new Paragraph();
+        paragrapheHeader1.add("hello");
+        document.add(paragrapheHeader1);
 
         document.close();
     }
